fc783f035da20e6f6584eef20628ecf74c7461c8
Lukáš Sitter <lukas.sitter@gmail.com>
2022-06-29 17:32:18 -0400

    Finish

---
+++ project/Omnifood/js/script.js
@@ -0,0 +1,45 @@
+console.log("hello world");
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear().toString();
+yearEl.textContent = currentYear;
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinksEl = document.querySelectorAll("a:link");
+
+allLinksEl.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+  });
+});
+
+function checkFlexBox() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexBox();
